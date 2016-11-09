c32ceb2efcea4d6a9852e2ef5eaf36de441d19ac
robert789i <robertoramos789@gmail.com>
2016-11-08 19:26:03 -0600

    New Class

---
+++ MVC-BNG/Controllers/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_BNG.Controllers
+{
+    public class Class1
+    {
+    }
+
+    public interface OnDrawerListener
+    {
+
+    }
+}
